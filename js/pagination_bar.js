d75d05c6b11e1a3bf410826658b186d77e457c7a
vitor <ckp_vitorr@hotmail.com>
2017-10-04 11:11:37 -0300

    converted jquery's ajax to angular's broadcast

--- js/pagination_bar.js
+++ js/pagination_bar.js
@@ -1,13 +1,36 @@
+var app = angular.module("pagination_bar", []);
+
+//receives number of pages, stores the current selected page and sends it
+app.controller("pagination_bar", function($scope, $rootScope){
+	$scope.active_index = 0; 
 	
-$(document).ready( function() {
+	$scope.getNumber = function(n) { //for ng-repeat, which only accept arrays
+		console.log("Pagination buttons created : " + n);
+		array = [];
+		for(i = 0; i <  n; i++){
+			array.push(i);
+		}
+		return array;
+	}
 	
-	$(".pagination_link").click(function(){
-		$.ajax({
-		  type: "POST",
-		  url: "session.php",
-		  data: { current_page: Number( $(this).text() ) },
-		  success: function(response) {location.reload(); }
-		});
+	$scope.number_of_pages = $scope.getNumber(1);
+	
+	$rootScope.$on("number_of_pages_update", function(event, args){
+		$scope.number_of_pages = $scope.getNumber(args)
+		console.log("ON number_of_pages_update : " + $scope.number_of_pages);
 	});
 	
+	$scope.curr_page_update = function(page_num){
+		$rootScope.$broadcast("curr_page_update", page_num);
+		console.log("curr_page_update : " + page_num);
+	}
+});
+
+app.controller("page-test", function($scope, $rootScope){
+	$scope.page_amtn = 0;
+	
+	$scope.number_of_pages_update = function(){
+		$rootScope.$broadcast("number_of_pages_update", $scope.page_amtn);
+		console.log("number_of_pages_update : " + $scope.page_amtn);
+	}
 });
--- pagination_bar.php
+++ pagination_bar.php
@@ -1,23 +1,27 @@
-<html>
-	<header>
-		<link rel="stylesheet" type="text/css" href="css/pagination_bar.css">
-		<script type="text/javascript"  src="js/pagination_bar.js"></script>
-	</header>
+<html ng-app="pagination_bar">
+	<head>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
+		<script src="js/pagination_bar.js"></script>
+		<link rel="stylesheet" type="text/css" href="../css/pagination_bar.css">
+	</head>
 	
-	<?php $number_of_links = $_SESSION['number_of_results'] / $_SESSION['max_cells'] ; ?>
 	<body>
-		<div class = "pagination_bar">
-			<a class = "arrow"> << </a>
-			<?php for($i = 1; $i <= $number_of_links; $i++) { ?>
-				
-				<?php if($i == $_SESSION['curr_page']) {?>
-					<a class = "pagination_link active"><?php echo $i;?></a>
-				<?php  continue; }; ?> 
-				<a class = "pagination_link"><?php echo $i; ?></a>
-				
-			<?php }; ?>
-			<a class = "arrow"> >> </a>
-			
+		
+		<div ng-controller="pagination_bar">
+			<div class = "pagination_bar"">
+				<a class = "arrow"> << </a>
+					<a ng-class = "[pagination_link, {'pagination_link active' : $index === active_index}]" 
+					ng-click="curr_page_update($index+1);" 
+					ng-repeat="n in number_of_pages track by $index">
+						{{$index+1}}
+					</a>
+				<a class = "arrow"> >> </a>
+			</div>	
+		<div>
+		
+		<div ng-controller="page-test">
+			<input input="number" min="0" placeholder="insira um numero" ng-model="page_amtn" ng-Change="number_of_pages_update()"></input>
 		</div>
-	<body>
+	</body>
 </html>
